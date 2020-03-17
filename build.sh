47f57f1eab5696b2dffd877cfbfa42559243bda0
yq wang <couragewang@126.com>
2020-03-17 14:43:46 +0800

    修改脚本信息

--- build.sh
+++ build.sh
@@ -2,19 +2,24 @@
 
 export DOCKER_CLI_EXPERIMENTAL=enabled
 
-modules=("node")
+SUDO="sudo "
+if [ "wangyongqi_123" == "$(uname -s)" ]; then
+    SUDO=""
+fi
+
+modules=( "node" )
 
 makeModule() {
-  for dir in ${modules[@]}; do
-    echo "------------------> Building module: ${dir}"
-    cd ${dir}
-    # Build linux dist
-    GOOS=linux GOARCH=arm make -f ../Makefile $*
-    GOOS=linux GOARCH=arm64 make -f ../Makefile $*
-    GOOS=linux GOARCH=amd64 make -f ../Makefile $*
-    GOOS=linux make -f ../Makefile manifest
-    cd -
-  done
+    for dir in ${modules[@]} ; do
+        echo "###### Building module: ${dir}"
+        cd ${dir}
+        # Build linux dist
+        OS_SUDO=${SUDO} GOOS=linux GOARCH=arm make -f ../Makefile $*
+        OS_SUDO=${SUDO} GOOS=linux GOARCH=arm64 make -f ../Makefile $*
+        OS_SUDO=${SUDO} GOOS=linux GOARCH=amd64 make -f ../Makefile $*
+        OS_SUDO=${SUDO} GOOS=linux make -f ../Makefile manifest
+        cd -
+    done
 }
 
 makeModule image push
